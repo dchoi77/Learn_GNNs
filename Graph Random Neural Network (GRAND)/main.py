554304377faaf37ae0c8273c7fd29774d0c264e6
Daeshik Choi <math.dchoi@gmail.com>
2022-02-28 00:36:23 -0500

    Update main.py

--- Graph Random Neural Network (GRAND)/main.py
+++ Graph Random Neural Network (GRAND)/main.py
@@ -1,1 +1,14 @@
-#
+import numpy as np 
+import torch as th 
+import torch.nn as nn 
+import dgl.function as fn 
+import torch.nn.functional as F
+
+def drop_node(feats, drop_rate, training):
+  if training:
+		n = feats.shape[0]
+    keep_rate = 1. - drop_rate
+		keep_rates = th.FloatTensor(np.full(n, keep_rate))
+		masks = th.bernoulli(keep_rates).unsqueeze(1)	        # shape: (n, 1)
+		feats = masks.to(feats.device) * feats / keep_rate
+	return feats
